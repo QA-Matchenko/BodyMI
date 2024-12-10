257084473614ee8785fd75deb115f658e4974c26
QA-Matchenko <d.mineyeva1994@gmail.com>
2024-12-10 17:48:58 +0300

    Delete BmiService.java

--- BmiService.java
+++
@@ -1,7 +0,0 @@
-public class BmiService {
-
-    public int calculate(int weightInKg, double heightInMeters) {
-        double bmi = weightInKg / (heightInMeters * heightInMeters);
-        return (int) bmi;
-    }
-}
